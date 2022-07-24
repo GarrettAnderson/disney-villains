453327a3683a5fd4079bddb5fd555ef412e9ec7c
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-07-24 13:14:13 -0400

    updated the html of the character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Character extends Component {
     render() {
         return (
-            <main>
+            <section>
                 <h3>{this.props.characterName}</h3>
                 <img src={this.props.characterImg} />
                 <p>lorem ipsum</p>
-            </main>
+            </section>
         );
     }
 }
